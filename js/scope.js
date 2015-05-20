889ee9214c9086527faada6624fc24ee76fc5d4d
jntakpe <j.ntakpe@gmail.com>
2015-05-20 14:18:44 +0200

    Start

---
+++ js/scope.js
@@ -0,0 +1,13 @@
+(function () {
+    angular
+        .module('scopeapp', [])
+        .controller('FirstCtrl', FirstCtrl)
+        .controller('SecondCtrl', SecondCtrl);
+
+
+    function FirstCtrl() {
+    }
+
+    function SecondCtrl() {
+    }
+})();
