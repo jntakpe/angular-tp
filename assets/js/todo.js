9e28fe61fe7343daf18784e3e6a4875fbc11a9dc
jntakpe <j.ntakpe@gmail.com>
2015-05-21 11:28:20 +0200

    Affichages des tâches

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,4 +2,14 @@ angular
     .module('todoapp', [])
     .controller('TodoCtrl', function TodoCtrl($scope) {
         $scope.remaining = 5;
+        $scope.todos = [
+            {
+                title: 'My first task',
+                completed: true
+            },
+            {
+                title: 'My second task',
+                completed: false
+            }
+        ];
     });
