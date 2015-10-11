f27fb5a4db90cc3bb4f6f57dc0a3bdb2f82377fc
jntakpe <j.ntakpe@gmail.com>
2015-10-12 00:04:49 +0200

    Closure

--- playground.js
+++ playground.js
@@ -1,35 +1,7 @@
-function User(name) {
-    this.name = name;
-    this.sayHello = function () {
-        console.log('Bonjour ' + name);
-    }
+for (var i = 0; i < 10; i++) {
+    (function (idx) {
+        setTimeout(function () {
+            console.log(idx);
+        }, i * 1000);
+    })(i);
 }
-
-User.prototype.sayMyName = function () {
-    console.log('Bonjour ' + this.name);
-};
-
-var jocelyn = new User('Jocelyn');
-var gauthier = new User('Gauthier');
-
-console.log(gauthier.sayHello === jocelyn.sayHello);
-console.log(gauthier.sayMyName === jocelyn.sayMyName);
-
-User.prototype.isAdmin = function () {
-    console.log(!!this.admin);
-};
-
-
-function SuperUser(name) {
-    User.call(this, name);
-    this.admin = true;
-}
-
-SuperUser.prototype = Object.create(User.prototype);
-SuperUser.prototype.constructor = SuperUser;
-
-var su = new SuperUser('SomeAdmin');
-su.sayMyName();
-su.isAdmin();
-jocelyn.isAdmin();
-
